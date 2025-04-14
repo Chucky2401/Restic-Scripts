01e63b835023283899446845ae7b50517fb7f74d
The Black Wizard <chuckydu24@gmail.com>
2025-04-14 14:11:45 +0200

    refactro(Tjvs.Restic): change indent size
    For: Get-ResticStats, Remove-Environment, Set-Environment and
    ConverTo-HashtableSize.
    Edit all headers

--- inc/modules/Tjvs.Restic/Public/func_Remove-Environment.ps1
+++ inc/modules/Tjvs.Restic/Public/func_Remove-Environment.ps1
@@ -1,51 +1,25 @@
 function Remove-Environment {
-    <#
-        .SYNOPSIS
-            Summary of the script
-        .DESCRIPTION
-            Script description
-        .PARAMETER param1
-            Parameter description
-        .INPUTS
-            Pipeline input data
-        .OUTPUTS
-            Output data
-        .EXAMPLE
-            .\template.ps1 param1
-        .NOTES
-            Name           : Script-Name
-            Version        : 1.0.0
-            Created by     : Chucky2401
-            Date Created   : 22/12/2022
-            Modify by      : Chucky2401
-            Date modified  : 22/12/2022
-            Change         : Creation
-    #>
-    
-    #---------------------------------------------------------[Script Parameters]------------------------------------------------------
-    
-    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low")]
-    Param (
-        #Script parameters go here
-    )
-    
-    #---------------------------------------------------------[Initialisations]--------------------------------------------------------
-    
-    #Set Error Action to Silently Continue
-    $ErrorActionPreference      = "SilentlyContinue"
-    
-    If ($PSBoundParameters['Debug']) {
-        $DebugPreference = "Continue"
-    } Else {
-        $DebugPreference = "SilentlyContinue"
-    }
-    
-    #-----------------------------------------------------------[Functions]------------------------------------------------------------
-        
-    #----------------------------------------------------------[Declarations]----------------------------------------------------------
-    
-    #-----------------------------------------------------------[Execution]------------------------------------------------------------
+  <#
+    .SYNOPSIS
+      Remove Retic environment variables
+    .DESCRIPTION
+      Remove from the current PowerShell session, restic environment variables: password and repository path
+    .EXAMPLE
+      Remove-Environment
+    .NOTES
+      Name           : Remove-Environment
+      Version        : 1.0.0
+      Created by     : Chucky2401
+      Date Created   : 22/12/2022
+      Modify by      : Chucky2401
+      Date modified  : 22/12/2022
+      Change         : Creation
+  #>
+  
+  [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low")]
+  Param (
+  )
 
-    Remove-Item Env:\RESTIC_PASSWORD
-    Remove-Item Env:\RESTIC_REPOSITORY
+  Remove-Item Env:\RESTIC_PASSWORD
+  Remove-Item Env:\RESTIC_REPOSITORY
 }
