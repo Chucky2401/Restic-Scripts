1333c8ff41be4d3572739d909b9d867db13f4871
The Black Wizard <chuckydu24@gmail.com>
2025-04-14 14:14:05 +0200

    refactor(Tjvs.Settings): change indent size
    All headers have been edit

--- inc/modules/Tjvs.Settings/func_Test-Settings.ps1
+++ inc/modules/Tjvs.Settings/func_Test-Settings.ps1
@@ -1,99 +1,82 @@
 function Test-Settings {
-    <#
-        .SYNOPSIS
-            Summary of the script
-        .DESCRIPTION
-            Script description
-        .PARAMETER param1
-            Parameter description
-        .INPUTS
-            Pipeline input data
-        .OUTPUTS
-            Output data
-        .EXAMPLE
-            .\template.ps1 param1
-        .NOTES
-            Name           : Script-Name
-            Version        : 1.0.0
-            Created by     : Chucky2401
-            Date Created   : 24/12/2022
-            Modify by      : Chucky2401
-            Date modified  : 24/12/2022
-            Change         : Creation
-    #>
-    
-    #---------------------------------------------------------[Script Parameters]------------------------------------------------------
-    
-    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low")]
-    Param (
-        [Parameter(Position = 0, Mandatory = $False)]
-        [PSObject]$Settings
+  <#
+    .SYNOPSIS
+      Validate the settings file
+    .DESCRIPTION
+      Get the user settings file and validate it match the template settings
+    .PARAMETER Settings
+      User settings get from settings.json
+    .OUTPUTS
+      Boolean
+    .EXAMPLE
+      Test-Settings -Settings $settings
+    .NOTES
+      Name           : Test-Settings
+      Version        : 1.0.0
+      Created by     : Chucky2401
+      Date Created   : 24/12/2022
+      Modify by      : Chucky2401
+      Date modified  : 24/12/2022
+      Change         : Creation
+  #>
+  
+  [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low")]
+  Param (
+    [Parameter(Position = 0, Mandatory = $False)]
+    [PSObject]$Settings
+  )
+  
+  #-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+  function Get-ObjectProperties {
+    [CmdletBinding()]
+    param(
+      [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
+      [PSObject]$Object,
+      [Parameter(Mandatory = $False)]
+      [Int32]$Level = 0,
+      [Parameter(Mandatory = $False)]
+      [String]$Parent = ""
     )
-    
-    #---------------------------------------------------------[Initialisations]--------------------------------------------------------
-    
-    #Set Error Action to Silently Continue
-    $ErrorActionPreference      = "SilentlyContinue"
 
-    If ($PSBoundParameters['Debug']) {
-        $DebugPreference = "Continue"
-    } Else {
-        $DebugPreference = "SilentlyContinue"
-    }
-    
-    #-----------------------------------------------------------[Functions]------------------------------------------------------------
+    $Tabulation         = ""
+    $FullPath           = ""
+    $PropertiesFullPath = @()
 
-    function Get-ObjectProperties {
-        [CmdletBinding()]
-        param(
-            [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
-            [PSObject]$Object,
-            [Parameter(Mandatory = $False)]
-            [Int32]$Level = 0,
-            [Parameter(Mandatory = $False)]
-            [String]$Parent = ""
-        )
-    
-        $Tabulation = ""
-        $FullPath = ""
-        $PropertiesFullPath = @()
-    
-        for ($i = 0 ; $i -lt $Level ; $i++) {
-            $Tabulation += "`t"
-        }
-    
-        If ($Parent -ne "") {
-            $FullPath = "$($Parent)."
-        }
-    
-        $Object | Get-Member -MemberType NoteProperty | Sort-Object -Property Name | ForEach-Object {
-            If ($PSItem.Definition -match 'PSCustomObject') {
-                #Write-Host "$($Tabulation)$($PSItem.Name)"
-                [String[]]$PropertiesFullPath += Get-ObjectProperties -Object $($Object.$($PSItem.Name)) -Level $($Level+1) -Parent $($PSItem.Name)
-            } Else {
-                #Write-Host "$($Tabulation)$($PSItem.Name) Value: $($Object.$($PSItem.Name))"
-                $PropertiesFullPath += "$($FullPath)$($PSItem.Name)"
-            }
-        }
-    
-        Return [String[]]$PropertiesFullPath
-    }
-    
-    #----------------------------------------------------------[Declarations]----------------------------------------------------------
-    
-    $bValidSettings = $True
-
-    $aTemplateProperties = $DefaultSettings | Get-ObjectProperties | Where-Object { $PSItem -notmatch "^__|\.__" }
-    $aProperties = $Settings | Get-ObjectProperties | Where-Object { $PSItem -notmatch "^__|\.__" }
-    
-    #-----------------------------------------------------------[Execution]------------------------------------------------------------
-    
-    $aMissingSettings = Compare-Object -ReferenceObject $aTemplateProperties -DifferenceObject $aProperties | Where-Object { $PSItem.SideIndicator -eq "<=" } `
-    | Measure-Object
-
-    If ([String[]]$aMissingSettings.Count -gt 0) {
-        $bValidSettings = $False
+    for ($i = 0 ; $i -lt $Level ; $i++) {
+      $Tabulation += "`t"
     }
 
-    Return $bValidSettings
+    If ($Parent -ne "") {
+        $FullPath = "$($Parent)."
+    }
+
+    $Object | Get-Member -MemberType NoteProperty | Sort-Object -Property Name | ForEach-Object {
+      If ($PSItem.Definition -match 'PSCustomObject') {
+        [String[]]$PropertiesFullPath += Get-ObjectProperties -Object $($Object.$($PSItem.Name)) -Level $($Level+1) -Parent $($PSItem.Name)
+      } Else {
+        $PropertiesFullPath += "$($FullPath)$($PSItem.Name)"
+      }
+    }
+
+    Return [String[]]$PropertiesFullPath
+  }
+  
+  #----------------------------------------------------------[Declarations]----------------------------------------------------------
+  
+  $bValidSettings = $True
+
+  $aTemplateProperties = $DefaultSettings | Get-ObjectProperties | Where-Object { $PSItem -notmatch "^__|\.__" }
+  $aProperties         = $Settings | Get-ObjectProperties | Where-Object { $PSItem -notmatch "^__|\.__" }
+  
+  #-----------------------------------------------------------[Execution]------------------------------------------------------------
+  
+  $aMissingSettings = Compare-Object -ReferenceObject $aTemplateProperties -DifferenceObject $aProperties | Where-Object { $PSItem.SideIndicator -eq "<=" } `
+  | Measure-Object
+
+  If ([String[]]$aMissingSettings.Count -gt 0) {
+    $bValidSettings = $False
+  }
+
+  Return $bValidSettings
 }
--- inc/modules/Tjvs.Settings/func_Test-SettingsFile.ps1
+++ inc/modules/Tjvs.Settings/func_Test-SettingsFile.ps1
@@ -1,16 +1,32 @@
 function Test-SettingsFile {
-    [CmdletBinding()]
-    param (
-    )
+  <#
+    .SYNOPSIS
+      Test settings file presence
+    .DESCRIPTION
+      Test if settings file '.\conf\settings.json' is present
+    .EXAMPLE
+      Test-SettingsFile
+    .NOTES
+      Name           : Test-SettingsFile
+      Version        : 1.0.0
+      Created by     : Chucky2401
+      Date Created   : 24/12/2022
+      Modify by      : Chucky2401
+      Date modified  : 24/12/2022
+      Change         : Creation
+  #>
+  [CmdletBinding()]
+  param (
+  )
 
-    $scriptRoot = $PSScriptRoot -replace "\\inc\\modules\\Tjvs.Settings", ""
+  $scriptRoot = $PSScriptRoot -replace "\\inc\\modules\\Tjvs.Settings", ""
 
-    Import-LocalizedData -BindingVariable "Message" -BaseDirectory "$($scriptRoot)\local" -FileName "Tjvs.Modules.psd1"
+  Import-LocalizedData -BindingVariable "Message" -BaseDirectory "$($scriptRoot)\local" -FileName "Tjvs.Modules.psd1"
 
-    If (-not (Test-Path ".\conf\settings.json")) {
-        Write-Warning $Message.NoSetFile
-        Write-Host $Message.PleaseAnswer
-        
-        New-Settings -RootPath $scriptRoot
-    }
+  If (-not (Test-Path ".\conf\settings.json")) {
+    Write-Warning $Message.NoSetFile
+    Write-Host $Message.PleaseAnswer
+    
+    New-Settings -RootPath $scriptRoot
+  }
 }
