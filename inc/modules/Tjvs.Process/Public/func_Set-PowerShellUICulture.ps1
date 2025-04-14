b4b55f8e6dc71e7c596d541525fff2ed13fbd9c3
The Black Wizard <chuckydu24@gmail.com>
2025-04-14 14:08:36 +0200

    refactor(Set-PowerShellUICulture): change indent size

--- inc/modules/Tjvs.Process/Public/func_Set-PowerShellUICulture.ps1
+++ inc/modules/Tjvs.Process/Public/func_Set-PowerShellUICulture.ps1
@@ -1,32 +1,29 @@
 function Set-PowerShellUICulture {
-    <#
-        .SYNOPSIS
-            Summary of the script
-        .DESCRIPTION
-            Script description
-        .PARAMETER param1
-            Parameter description
-        .INPUTS
-            Pipeline input data
-        .OUTPUTS
-            Output data
-        .EXAMPLE
-            .\template.ps1 param1
-        .NOTES
-            Name           : Script-Name
-            Version        : 1.0.0
-            Created by     : Chucky2401
-            Date Created   : 23/12/2022
-            Modify by      : Chucky2401
-            Date modified  : 23/12/2022
-            Change         : Creation
-    #>
-    
-    Param (
-        [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
-        [System.Globalization.CultureInfo]$Culture
-    )
+  <#
+    .SYNOPSIS
+      Change current thread culture
+    .DESCRIPTION
+      Change the current PowerShell thread (console) culture from the code with the format xx-XX
+    .PARAMETER Culture
+      Culture code of the language.
+      Format 'xx-XX' as a string can be use
+    .EXAMPLE
+      Set-PowerShellUICulture -Culture en-US
+    .NOTES
+      Name           : Set-PowerShellUICulture
+      Version        : 1.0.0
+      Created by     : Chucky2401
+      Date Created   : 23/12/2022
+      Modify by      : Chucky2401
+      Date modified  : 23/12/2022
+      Change         : Creation
+  #>
+  
+  Param (
+    [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
+    [System.Globalization.CultureInfo]$Culture
+  )
 
-    [System.Threading.Thread]::CurrentThread.CurrentUICulture = $Culture
-    [System.Threading.Thread]::CurrentThread.CurrentCulture = $Culture
+  [System.Threading.Thread]::CurrentThread.CurrentUICulture = $Culture
+  [System.Threading.Thread]::CurrentThread.CurrentCulture = $Culture
 }
